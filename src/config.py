2012ea6b3655cbe18818616e3a522ebbfb778148
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2024-10-28 22:46:55 +0700

    Edited by gitignore

---
+++ src/config.py
@@ -0,0 +1,7 @@
+API_KEY = "a77ab53eb3msh6b33b6bb431caeap15562ejsn43d0f0ffad82"
+BASE_URL = "https://weatherapi-com.p.rapidapi.com"
+HEADERS = {
+    "x-rapidapi-key": API_KEY,
+    "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+}
+cities = ["Томск", "Абакан", "Екатеринбург", "Москва"]
